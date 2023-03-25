2db303a6d50350c834e1ab5d489575cacaf7a962
Yunus <yunusozdemir468@gmail.com>
2023-03-26 00:15:14 +0300

    fix

--- Business/MicroServiceTemplate.Domain/Enums/ResultStatus.cs
+++ Business/MicroServiceTemplate.Domain/Enums/ResultStatus.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-namespace MicroServiceTemplate.Domain.Enums
-{
-    public enum ResultStatus
-    {
-        Success = 0,
-        Info = 1,
-        Warning = 2,
-        Error = 3
-    }
-}
 
+namespace MicroServiceTemplate.Domain.Enums;
+
+public enum ResultStatus
+{
+    Success = 0,
+    Info = 1,
+    Warning = 2,
+    Error = 3
+}
